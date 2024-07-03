d0e91ce47b3cc0070f497f55fea391798ec13fe0
Serena <serenalin0420@gmail.com>
2024-07-03 15:13:02 +0800

    add port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 4000;
 const cookieParser = require("cookie-parser");
 app.set("view engine", "pug");
 
